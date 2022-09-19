8e0df1de22fc13e633fb8c70b8f669f6fcad7640
AhmedFawzy900 <101644362+AhmedFawzy900@users.noreply.github.com>
2022-09-19 12:57:01 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
 module.exports = {
   pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
