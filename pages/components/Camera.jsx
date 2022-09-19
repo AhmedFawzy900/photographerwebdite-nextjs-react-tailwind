e12cfc67a60c77bf46794479507ba2d779bfee81
AhmedFawzy900 <101644362+AhmedFawzy900@users.noreply.github.com>
2022-09-19 12:35:52 +0200

    Update Camera.jsx

--- pages/components/Camera.jsx
+++ pages/components/Camera.jsx
@@ -5,21 +5,7 @@ const Camera = () => {
     <div>
         <h2 className='font-bold text-3xl py-10 text-center'>Tools Used</h2>
         <div className='flex flex-wrap w-full items-center justify-center'>
-            {cameraData.map((item) => {
-                return(
-                    <div>
-                        <div className='m-2 hidden sm:block ss:block ' key={item.id}>
-                            {item.id === 6 
-                            ? <Image src={item.image} alt='/' width='614' height='300' objectFit='cover' /> 
-                            : <Image src={item.image} alt='/' width='300' height='300' objectFit='cover' /> }
-                            
-                        </div>
-                        <div className='m-2  block sm:hidden ss:hidden' key={item.id}>
-                        <Image src={item.image} alt='/' width='200' height='170' objectFit='cover' />   
-                        </div>
-                    </div>
-                )
-            })}
+          
         </div>
     </div>
   )
